f819b78d1bff7ad9f084e3531a5ce43df2737760
Floyd <wogus392@gmail.com>
2024-10-06 00:55:21 +0900

    feat: add domain

--- next.config.mjs
+++ next.config.mjs
@@ -5,7 +5,7 @@ const nextConfig = {
   // Configure `pageExtensions` to include MDX files
   pageExtensions: ["js", "jsx", "mdx", "ts", "tsx"],
   images: {
-    domains: ["i.pinimg.com"],
+    domains: ["i.pinimg.com", "cdn.pixabay.com"],
   },
   // Optionally, add any other Next.js config below
 };
